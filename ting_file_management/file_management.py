735768cb8510a14ccb13a2b6384c1b4eb398b6c4
Felipe Dias <felipeferreiradias1@gmail.com>
2022-05-12 17:23:42 -0300

    :sparkles: Feat: build file management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, 'r') as file:
+            return [line.strip() for line in file]
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+            
