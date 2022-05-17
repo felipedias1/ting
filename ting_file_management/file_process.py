092df6fc75e518e220b2b638ee1c5aeab36d5fa9
Felipe Dias <felipeferreiradias1@gmail.com>
2022-05-16 21:19:41 -0300

    Feat: build functions to search word

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,7 +3,10 @@ from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance):
-    file_exists = True if path_file in instance.data else False
+    file_exists = False
+    for element in instance.data:
+        if path_file == element["nome_do_arquivo"]:
+            file_exists = True
     lines = txt_importer(path_file)
     previous_process = {
         "nome_do_arquivo": path_file,
@@ -11,20 +14,20 @@ def process(path_file, instance):
         "linhas_do_arquivo": lines
     }
     if file_exists is False:
-        instance.enqueue(path_file)
+        instance.enqueue(previous_process)
     sys.stdout.write(f"{previous_process}\n")
 
 
 def remove(instance):
     if instance.__len__() == 0:
         return sys.stdout.write('Não há elementos\n')
-    remove = instance.dequeue()
+    remove = instance.dequeue()['nome_do_arquivo']
     return sys.stdout.write(f"Arquivo {remove} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
     try:
         file = instance.search(position)
-        process(file, instance)
+        process(file['nome_do_arquivo'], instance)
     except IndexError:
         sys.stderr.write('Posição inválida\n')
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,42 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    if len(word) < 1:
+        return None
+    occurrences = []
+    for file in instance.data:
+        print(">>>>>>", file )
+        occurrences_lines = []
+        for index, line in enumerate(file['linhas_do_arquivo']):
+            if word.lower() in line.lower(): 
+                occurrences_lines.append({
+                    'linha': index + 1
+                })
+        if len(occurrences_lines) > 0:
+            occurrences.append({
+                'palavra': word,
+                'arquivo': file['nome_do_arquivo'],
+                'ocorrencias': occurrences_lines
+            })
+    return occurrences
+    
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    if len(word) < 1:
+        return None
+    occurrences = []
+    for file in instance.data:
+        occurrences_lines = []
+        for index, line in enumerate(file['linhas_do_arquivo']):
+            if word.lower() in line.lower(): 
+                occurrences_lines.append({
+                    'conteudo': line,
+                    'linha': index + 1
+                })
+        if len(occurrences_lines) > 0:
+            occurrences.append({
+                'palavra': word,
+                'arquivo': file['nome_do_arquivo'],
+                'ocorrencias': occurrences_lines
+            })
+    print(">>>>>>>>>>>>", occurrences)
+    return occurrences
