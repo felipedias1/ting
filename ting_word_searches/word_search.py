01512a402d04f1997168114e458ea9b4e621fe5a
Felipe Dias <felipeferreiradias1@gmail.com>
2022-05-16 21:22:04 -0300

    :rotating_light Fix: Fix Flake errors

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,12 +1,9 @@
 def exists_word(word, instance):
-    if len(word) < 1:
-        return None
     occurrences = []
     for file in instance.data:
-        print(">>>>>>", file )
         occurrences_lines = []
         for index, line in enumerate(file['linhas_do_arquivo']):
-            if word.lower() in line.lower(): 
+            if word.lower() in line.lower():
                 occurrences_lines.append({
                     'linha': index + 1
                 })
@@ -17,17 +14,14 @@ def exists_word(word, instance):
                 'ocorrencias': occurrences_lines
             })
     return occurrences
-    
 
 
 def search_by_word(word, instance):
-    if len(word) < 1:
-        return None
     occurrences = []
     for file in instance.data:
         occurrences_lines = []
         for index, line in enumerate(file['linhas_do_arquivo']):
-            if word.lower() in line.lower(): 
+            if word.lower() in line.lower():
                 occurrences_lines.append({
                     'conteudo': line,
                     'linha': index + 1
@@ -38,5 +32,4 @@ def search_by_word(word, instance):
                 'arquivo': file['nome_do_arquivo'],
                 'ocorrencias': occurrences_lines
             })
-    print(">>>>>>>>>>>>", occurrences)
     return occurrences
